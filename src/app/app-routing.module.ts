9e9a0af4d534afbeab7ed7a9062e98159fd68e1e
Lyrrad96 <darryl.r.andrade@gmail.com>
2024-01-27 15:50:57 +0530

    Added ag-grid installation and component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HscrollComponent } from './hscroll/hscroll.component';
+import { GridComponent } from './grid/grid.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'grid', component: GridComponent},
   {path: 'hscroll', component: HscrollComponent},
-  { path: '**',   redirectTo: '/hscroll', pathMatch: 'full' }
+  { path: '**',   redirectTo: '/grid', pathMatch: 'full' }
 ];
 
 @NgModule({
